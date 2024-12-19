ca9612982d39a3ebf7946c86ce7a9257f8e19693
Connor Boyle <connor.bo@gmail.com>
2024-12-18 17:11:10 -0800

    Add route to create empty poll

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::string::ToString;
+use std::sync::{Arc, Mutex};
 use axum::{routing::get, Json, Router};
 use axum::extract::State;
+use axum::http::Response;
 use serde::Serialize;
 
 #[derive(Clone)]
 struct AppState {
-    polls: Vec<Poll>
+    polls: Arc<Mutex<Vec<Poll>>>
 }
 
 #[derive(Clone, Serialize)]
@@ -13,24 +15,38 @@ struct Poll {
     candidates: Vec<String>
 }
 
-async fn handler (
+async fn list_polls (
     // access the state via the `State` extractor
     // extracting a state of the wrong type results in a compile error
     State(state): State<AppState>,
 ) -> Json<Vec<Poll>> {
-    Json(state.polls)
+    Json(state.polls.lock().unwrap().to_vec())
+}
+
+async fn create_poll (
+    State(state): State<AppState>,
+) {
+    let mut polls = state.polls.lock().unwrap();
+    polls.push(Poll {candidates: Vec::new()});
 }
 
 #[tokio::main]
 async fn main() {
-    let state = AppState {polls: vec![Poll {candidates: vec![
-        "O'Brien".to_string(),
-        "Murphy".to_string()
-    ]}]};
+    let state = AppState {
+        polls: Arc::new(Mutex::new(
+            vec![Poll {
+                candidates: vec![
+                    "O'Brien".to_string(),
+                    "Murphy".to_string()
+                ]
+            }]
+        ))
+    };
 
     // create a `Router` that holds our state
     let app = Router::new()
-        .route("/", get(handler))
+        .route("/", get(list_polls))
+        .route("/polls", get(create_poll))
         // provide the state so the router can access it
         .with_state(state);
 
