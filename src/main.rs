2aafdd7c189e1250603860a5547c778f6255bab8
Connor Boyle <connor.bo@gmail.com>
2024-12-29 13:39:12 -0800

    Add detail view and delete view for polls

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::sync::{Arc, Mutex};
 use axum::{routing::get, Json, Router};
 use axum::extract::{Path, State};
 use axum::http::StatusCode;
-use axum::routing::post;
+use axum::routing::{delete, post};
 use models::{Poll, VotingError};
 use crate::models::PollMetadata;
 
@@ -34,6 +34,32 @@ async fn create_poll (
         poll_metadata.min_score,
         poll_metadata.max_score
     ));
+}
+
+#[axum::debug_handler]
+async fn get_poll (
+    State(state): State<AppState>,
+    Path(poll_id): Path<usize>,
+) -> Result<Json<PollMetadata>, (StatusCode, String)> {
+    let polls = state.polls.lock().unwrap();
+    let poll_option = polls.get(poll_id);
+    match poll_option {
+        Some(poll) => { Ok(Json(poll.metadata.clone())) }
+        None => { Err((StatusCode::NOT_FOUND, format!("poll with id {} not found", poll_id))) }
+    }
+}
+
+#[axum::debug_handler]
+async fn delete_poll (
+    State(state): State<AppState>,
+    Path(poll_id): Path<usize>,
+) -> Result<(), (StatusCode, String)> {
+    let mut polls = state.polls.lock().unwrap();
+    if poll_id < 0 || poll_id >= polls.len() {
+        return Err((StatusCode::NOT_FOUND, format!("poll with id {} not found", poll_id)));
+    }
+    polls.remove(poll_id);
+    Ok(())
 }
 
 async fn add_vote (
@@ -77,8 +103,10 @@ async fn main() {
 
     // create a `Router` that holds our state
     let app = Router::new()
-        .route("/polls", get(list_polls))
         .route("/polls", post(create_poll))
+        .route("/polls", get(list_polls))
+        .route("/polls/:poll_id", get(get_poll))
+        .route("/polls/:poll_id", delete(delete_poll))
         .route("/polls/:poll_id/votes", post(add_vote))
         // provide the state so the router can access it
         .with_state(state);
